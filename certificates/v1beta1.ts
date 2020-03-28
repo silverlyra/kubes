ce94abaf5e8afb496311c491aa8d5a8aa7ec916b
Lyra Naeseth <lyra@lyra.codes>
2020-03-28 13:39:26 -0700

    Create types for v1.18

---
+++ certificates/v1beta1.ts
@@ -0,0 +1,122 @@
+import {ListMeta, ObjectMeta, Time} from "../meta/v1.ts"
+
+/**
+ * Describes a certificate signing request
+ */
+export interface CertificateSigningRequest {
+  /**
+   * APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+   */
+  apiVersion?: "certificates.k8s.io/v1beta1"
+
+  /**
+   * Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+   */
+  kind?: "CertificateSigningRequest"
+  metadata?: ObjectMeta
+
+  /**
+   * The certificate request itself and any additional information.
+   */
+  spec?: CertificateSigningRequestSpec
+
+  /**
+   * Derived information about the request.
+   */
+  status?: CertificateSigningRequestStatus
+}
+
+export interface CertificateSigningRequestCondition {
+  /**
+   * timestamp for the last update to this condition
+   */
+  lastUpdateTime?: Time
+
+  /**
+   * human readable message with details about the request state
+   */
+  message?: string
+
+  /**
+   * brief reason for the request state
+   */
+  reason?: string
+
+  /**
+   * request approval state, currently Approved or Denied.
+   */
+  type: string
+}
+
+export interface CertificateSigningRequestList {
+  /**
+   * APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+   */
+  apiVersion?: "certificates.k8s.io/v1beta1"
+  items: Array<CertificateSigningRequest>
+
+  /**
+   * Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+   */
+  kind?: "CertificateSigningRequestList"
+  metadata?: ListMeta
+}
+
+/**
+ * This information is immutable after the request is created. Only the Request and Usages fields can be set on creation, other fields are derived by Kubernetes and cannot be modified by users.
+ */
+export interface CertificateSigningRequestSpec {
+  /**
+   * Extra information about the requesting user. See user.Info interface for details.
+   */
+  extra?: {[name: string]: Array<string>}
+
+  /**
+   * Group information about the requesting user. See user.Info interface for details.
+   */
+  groups?: Array<string>
+
+  /**
+   * Base64-encoded PKCS#10 CSR data
+   */
+  request: string
+
+  /**
+   * Requested signer for the request. It is a qualified name in the form: `scope-hostname.io/name`. If empty, it will be defaulted:
+   *  1. If it's a kubelet client certificate, it is assigned
+   *     "kubernetes.io/kube-apiserver-client-kubelet".
+   *  2. If it's a kubelet serving certificate, it is assigned
+   *     "kubernetes.io/kubelet-serving".
+   *  3. Otherwise, it is assigned "kubernetes.io/legacy-unknown".
+   * Distribution of trust for signers happens out of band. You can select on this field using `spec.signerName`.
+   */
+  signerName?: string
+
+  /**
+   * UID information about the requesting user. See user.Info interface for details.
+   */
+  uid?: string
+
+  /**
+   * allowedUsages specifies a set of usage contexts the key will be valid for. See: https://tools.ietf.org/html/rfc5280#section-4.2.1.3
+   *      https://tools.ietf.org/html/rfc5280#section-4.2.1.12
+   */
+  usages?: Array<string>
+
+  /**
+   * Information about the requesting user. See user.Info interface for details.
+   */
+  username?: string
+}
+
+export interface CertificateSigningRequestStatus {
+  /**
+   * If request was approved, the controller will place the issued certificate here.
+   */
+  certificate?: string
+
+  /**
+   * Conditions applied to the request, such as approval or denial.
+   */
+  conditions?: Array<CertificateSigningRequestCondition>
+}
---
+++ settings/v1alpha1.ts
@@ -0,0 +1,74 @@
+import {EnvFromSource, EnvVar, Volume, VolumeMount} from "../core/v1.ts"
+import {LabelSelector, ListMeta, ObjectMeta} from "../meta/v1.ts"
+
+/**
+ * PodPreset is a policy resource that defines additional runtime requirements for a Pod.
+ */
+export interface PodPreset {
+  /**
+   * APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+   */
+  apiVersion?: "settings.k8s.io/v1alpha1"
+
+  /**
+   * Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+   */
+  kind?: "PodPreset"
+  metadata?: ObjectMeta
+  spec?: PodPresetSpec
+}
+
+/**
+ * PodPresetList is a list of PodPreset objects.
+ */
+export interface PodPresetList {
+  /**
+   * APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+   */
+  apiVersion?: "settings.k8s.io/v1alpha1"
+
+  /**
+   * Items is a list of schema objects.
+   */
+  items: Array<PodPreset>
+
+  /**
+   * Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+   */
+  kind?: "PodPresetList"
+
+  /**
+   * Standard list metadata. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+   */
+  metadata?: ListMeta
+}
+
+/**
+ * PodPresetSpec is a description of a pod preset.
+ */
+export interface PodPresetSpec {
+  /**
+   * Env defines the collection of EnvVar to inject into containers.
+   */
+  env?: Array<EnvVar>
+
+  /**
+   * EnvFrom defines the collection of EnvFromSource to inject into containers.
+   */
+  envFrom?: Array<EnvFromSource>
+
+  /**
+   * Selector is a label query over a set of resources, in this case pods.
+   */
+  selector?: LabelSelector
+
+  /**
+   * VolumeMounts defines the collection of VolumeMount to inject into containers.
+   */
+  volumeMounts?: Array<VolumeMount>
+
+  /**
+   * Volumes defines the collection of Volume to inject into the pod.
+   */
+  volumes?: Array<Volume>
+}
